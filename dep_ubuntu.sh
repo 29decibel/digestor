336c740ebb6f75b547628826f15a5a8105c28fc2
mike li <mike.li@acxiom.com>
2014-02-10 16:56:23 -0800

    update dependecies install script

--- dep_ubuntu.sh
+++ dep_ubuntu.sh
@@ -4,7 +4,8 @@
 sudo apt-get install -y libonig-dev
 
 # go libs
-go get github.com/moovweb/gokogiri
-go get github.com/jordan-wright/email
-go get github.com/mrjones/oauth
-go get github.com/moovweb/rubex
+go get
+# go get github.com/moovweb/gokogiri
+# go get github.com/jordan-wright/email
+# go get github.com/mrjones/oauth
+# go get github.com/moovweb/rubex
